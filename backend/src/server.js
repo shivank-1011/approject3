027aeafc0f9e8793deed25a1b44f7744f3115dfc
= <=>
2025-10-31 19:17:49 +0530

    initialised server.js

---
+++ backend/src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+import authRoutes from "./src/routes/authRoutes.js";
+
+dotenv.config();
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Expense Splitter API Running.");
+});
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, async () => {
+  try {
+    await prisma.$connect();
+    console.log("Connected to Database");
+    console.log(`Server running on http://localhost:${PORT}`);
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+});
